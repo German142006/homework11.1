35a464ef85dc6ad1d36b58d6345c40bdf3f16b77
German142006 <152334974+German142006@users.noreply.github.com>
2024-07-01 15:16:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('pifagor-table-container');
+    const table = document.createElement('table');
+
+    for (let i = 0; i <= 10; i++) {
+        const row = document.createElement('tr');
+        for (let j = 0; j <= 10; j++) {
+            const cell = i === 0 || j === 0 ? document.createElement('th') : document.createElement('td');
+            if (i === 0 && j === 0) {
+                cell.textContent = '';
+            } else if (i === 0) {
+                cell.textContent = j;
+            } else if (j === 0) {
+                cell.textContent = i;
+            } else {
+                cell.textContent = i * j;
+            }
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
+
+    container.appendChild(table);
+});
